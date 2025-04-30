bbd38045e0ac023df2c16b8afad4de4db5356c0e
Abed Jaber <abed.jaber81@gmail.com>
2025-04-30 15:13:28 +0300

    Add project files.

---
+++ Controllers/JobListing.cs
@@ -0,0 +1,42 @@
+﻿using ConsoleApp1;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+namespace Jobs.Controllers
+    {
+    public class JobListing : Controller
+        {
+        public IActionResult Index()
+            {
+            return View();
+            }
+        public IActionResult ShowDataButton()
+            {
+            return View();
+            }
+        public async Task<IActionResult> GetData()
+            {
+            var objects = await FetchDataFromApi();
+            return Json(objects.data);
+            }
+        private async Task<Root> FetchDataFromApi()
+            {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://www.arbeitnow.com/api/job-board-api");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            var response = client.GetStringAsync(client.BaseAddress).Result;
+            Root root = JsonConvert.DeserializeObject<Root>(response);
+            for (int j = 0; j < root.data.Count; j++)
+                {
+                root.data[j].created_at_date = ConvertTimestampToDateTime(root.data[j].created_at);
+                }
+            return root;
+            }
+        private DateTime ConvertTimestampToDateTime(long timestamp)
+            {
+            DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(timestamp);
+            return dateTimeOffset.LocalDateTime;
+            }
+        }
+    }
---
+++ Root.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+    {
+    public class Datum
+        {
+        public string slug { get; set; }
+        public string company_name { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public bool remote { get; set; }
+        public string url { get; set; }
+        public List<string> tags { get; set; }
+        public List<string> job_types { get; set; }
+        public string location { get; set; }
+        public int created_at { get; set; }
+        public DateTime created_at_date { get; set; }
+
+        }
+
+    public class Links
+        {
+        public string first { get; set; }
+        public object last { get; set; }
+        public object prev { get; set; }
+        public string next { get; set; }
+        }
+
+    public class Meta
+        {
+        public int current_page { get; set; }
+        public int from { get; set; }
+        public string path { get; set; }
+        public int per_page { get; set; }
+        public int to { get; set; }
+        public string terms { get; set; }
+        public string info { get; set; }
+        }
+
+    public class Root
+        {
+        public List<Datum> data { get; set; }
+        public Links links { get; set; }
+        public Meta meta { get; set; }
+        }
+
+
+    }
